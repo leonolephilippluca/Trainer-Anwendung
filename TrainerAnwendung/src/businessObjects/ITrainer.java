81b8017ac146f248b963dc1e9156bd9a51a473c8
fa15wagner <fa15wagner@W101_59.szut.uni-bremen.de>
2016-09-01 12:53:30 +0200

    init

---
+++ TrainerAnwendung/src/businessObjects/ITrainer.java
@@ -0,0 +1,11 @@
+package businessObjects;
+
+public interface ITrainer {
+	public int getId();
+	public String getName();
+	public void setName(String name);
+	public int getAlter();
+	public void setAlter(int age);
+	public int getErfahrung();
+	public void setErfahrung(int exp);
+}
---
+++ TrainerAnwendung/src/dataAccessObjects/ITrainerDao.java
@@ -0,0 +1,21 @@
+package dataAccessObjects;
+
+import java.util.List;
+
+import businessObjects.ITrainer;
+
+
+
+public interface ITrainerDao {
+	
+	public ITrainer create();
+	public void delete(ITrainer trainer);
+	public ITrainer first();
+	public ITrainer last();
+	public ITrainer next(ITrainer trainer);
+	public ITrainer previous(ITrainer trainer);
+	public void safe(ITrainer trainer);
+	public void select(List<ITrainer> trainerList);
+	public ITrainer select(int id);
+	
+}
