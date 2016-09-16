fe559df95285a95cae63accf7034b31105eb22ba
Leon Nonnast <Leon Nonnast@MSI-Notebook>
2016-09-16 11:52:39 +0200

    DataKlasse DB SQLite

--- TrainerAnwendung/src/dataAccessObjects/sqlite/TrainerDaoSqlite.java
+++ TrainerAnwendung/src/dataAccessObjects/sqlite/TrainerDaoSqlite.java
@@ -2,15 +2,17 @@ package dataAccessObjects.sqlite;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
-
 import businessObjects.ITrainer;
 import dataAccessObjects.interfaces.ITrainerDao;
+import dataLayer.Trainer;
 
 public class TrainerDaoSqlite implements ITrainerDao {
 
-	
+	//get DatabaseConnection SQLite
 	private Connection getConnection()
 	{
 		Connection conn = null;
@@ -37,52 +39,132 @@ public class TrainerDaoSqlite implements ITrainerDao {
 		
 		return conn;
 	}
+	
+	public ResultSet getExecuteReader(String query)
+	{
+		Statement stmt = null;
+		ResultSet reader = null;
+		Connection conn = getConnection();		
 
+		try {
+			stmt = conn.createStatement();
+			reader = stmt.executeQuery(query);
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return reader;
+	}
+	
+	public ITrainer getTrainerByReader(ResultSet reader)
+	{
+		ITrainer trainer = new Trainer();
+		try {
+			while( reader.next())
+			{
+				trainer.setExperience(reader.getInt("experience"));
+				trainer.setAge(reader.getInt("age"));
+				trainer.setName(reader.getString("name"));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return trainer;
+	}
 	@Override
 	public ITrainer create() {
 		// TODO Auto-generated method stub
 		
+		//erstellt eine Instance vom Objekt Trainer
 		return new Trainer();
 	}
 	@Override
 	public void delete(ITrainer trainer) {
 		// TODO Auto-generated method stub
+		Connection conn = this.getConnection();
 		
 	}
 	@Override
 	public ITrainer first() {
 		// TODO Auto-generated method stub
-		return null;
+		
+			String query = "SELECT TOP 1 * FROM TRAINER";
+			
+		return this.getTrainerByReader(this.getExecuteReader(query));
 	}
 	@Override
 	public ITrainer last() {
 		// TODO Auto-generated method stub
-		return null;
+		String query ="";
+		
+			ResultSet reader = this.getExecuteReader(query);
+
+			return this.getTrainerByReader(this.getExecuteReader(query));
 	}
 	@Override
 	public ITrainer next(ITrainer trainer) {
 		// TODO Auto-generated method stub
-		return null;
+		
+			String query = "SELECT * FROM TRAINER WHERE ID = '" + trainer.getId() + 1 + "'";
+
+			return this.getTrainerByReader(this.getExecuteReader(query));
 	}
 	@Override
 	public ITrainer previous(ITrainer trainer) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
 	@Override
 	public void safe(ITrainer trainer) {
 		// TODO Auto-generated method stub
-		Connection conn = getConnection();
-		
+		String query = null;
+		Statement stmt = null;
+		Connection conn = getConnection();		//Insert Trainer in die DB
+
+		try {
+			stmt = conn.createStatement();
+
+			query = "INSERT INTO TRAINER (AGE,EXPERIENCE,ID,NAME)" +
+					" VALUES('"+ trainer.getAge() +"','"+ trainer.getExperience()  +"','"+ trainer.getId()  +"','"+ trainer.getName() +"');";
+
+			stmt.executeQuery(query);
+
+
+			stmt.close();
+
+			conn.close();
+		} 
+		catch (SQLException e) 
+		{
+			System.err.println("Fehlern beim Speichern auf die Datenbank");
+			e.printStackTrace();
+		}
+
 	}
 	@Override
 	public void select(List<ITrainer> trainerList) {
 		// TODO Auto-generated method stub
+		// was soll hier gemacht werden?
+		// vielleicht save anstatt select?
+		String query = null;
+		Statement stmt = null;
+		ResultSet reader = null;
+		Connection conn = getConnection();		
 		
+		for(int i = 0; i< trainerList.size(); i++)
+		{
+			this.select(trainerList.get(i).getId());
+		}		
 	}
 	@Override
 	public ITrainer select(int id) {
 		// TODO Auto-generated method stub
-		return null;
+		String query  = "SELECT * FROM TRAINER WHERE ID EQ '"+ id +"';";
+		
+		return this.getTrainerByReader(this.getExecuteReader(query));
 	}
 }
--- TrainerAnwendung/src/dataAccessObjects/xml/DataLayerXml.java
+++ TrainerAnwendung/src/dataAccessObjects/xml/DataLayerXml.java
@@ -1,16 +1,58 @@
 package dataAccessObjects.xml;
 
 import java.util.List;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.*;
+import java.io.*;
 
 import businessObjects.ITrainer;
 import dataAccessObjects.interfaces.ITrainerDao;
+import dataLayer.Trainer;
 
 public class DataLayerXml implements ITrainerDao {
 
+	private Document createDoc()
+	{
+		DocumentBuilder builder = null;
+		ByteArrayInputStream input = null;
+		Document doc = null;
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+
+		try 
+		{
+			builder = factory.newDocumentBuilder();
+		} catch (ParserConfigurationException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		StringBuilder xmlStringBuilder = new StringBuilder();
+		
+		xmlStringBuilder.append("<?cml version='1.0'?> <class></class>");
+		try 
+		{
+			input = new ByteArrayInputStream(xmlStringBuilder.toString().getBytes("UTF-8"));
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try 
+		{
+			doc = builder.parse(input);
+		} catch (SAXException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return doc;		
+	}
+	
+	
+	
 	@Override
 	public ITrainer create() {
 		// TODO Auto-generated method stub
-		return null;
+		return new Trainer();
 	}
 
 	@Override
